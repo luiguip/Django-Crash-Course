7e14fc85ef9e874839e0b2897ff77116109e7d60
Luigui de A <luisguilhermepdea@gmail.com>
2021-01-30 22:41:20 -0300

    Lesson 6

--- accounts/models.py
+++ accounts/models.py
@@ -10,6 +10,12 @@ class Customer(models.Model):
     def __str__(self):
         return '[ name: ' + self.name + ' phone: ' + self.phone + ' email: ' + self.email + ' ]'
 
+class Tag(models.Model):
+    name = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return '[ name: ' + self.name + ' ]'
+
 class Product(models.Model):
     CATEGORY = (('Indoor', 'Indoor'),
         ('Out Door', 'Out Door'),)
@@ -17,12 +23,23 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, null=True)
     category = models.CharField(max_length=255, null=True, choices=CATEGORY)
     description = models.CharField(max_length=255, null=True)
+    tags = models.ManyToManyField(Tag)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     date_updated = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return '[ name: ' + self.name + ' price: ' + str(self.price) + \
+               ' category: ' + self.category + ' tags: ' + str(self.tags) + ' ]'
 
 class Order(models.Model):
     STATUS = (('Pending', 'Pending'),
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),)
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=True, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=255, null=True, choices=STATUS)
+
+    def __str__(self):
+        return '[ customer name: ' + self.customer.name + ' productName: ' + \
+               self.product.name + ' status: ' + self.status + ' ]'
